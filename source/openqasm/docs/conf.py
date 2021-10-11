6d9e7129320a75d866a40753f69ab220b41765f6
Jake Lishman <jake.lishman@ibm.com>
2021-10-11 13:43:53 +0100

    Remove empty directories from Sphinx configuration
    The `_templates` and `_static` directories have no content at the
    moment, so Git generally will not track them.  We have no need for them
    right now, so they can just be removed from the Sphinx configuration
    until they are needed.

--- source/openqasm/docs/conf.py
+++ source/openqasm/docs/conf.py
@@ -12,9 +12,6 @@ extensions = [
     "sphinx.ext.autodoc",
 ]
 
-templates_path = ['_templates']
-
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'alabaster'
-html_static_path = ['_static']
